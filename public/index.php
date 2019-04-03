81bcf83e803747ee0804798f4a8fad869755a41d
sacha <sacha6623@gmail.com>
2019-04-03 19:49:29 +0200

    Ajout composer et theme bootstrap

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 require '../config/dev.php';
 require '../config/Autoloader.php';
+require '../public/vendor/autoload.php';
 \App\config\Autoloader::register();
 
 $router = new \App\config\Router();
